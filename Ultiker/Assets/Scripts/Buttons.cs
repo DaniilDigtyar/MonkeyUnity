8e0a5d0add3e3ffa24a92422f732c09d887d5001
Daniil Digtyar <daniildigtyar@gmail.com>
2018-04-20 11:22:56 +0200

    Finished menus

---
+++ Ultiker/Assets/Scripts/Buttons.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour {
+
+    public void ButtonInstruccions()
+    {
+        Invoke("Instruccions", 0.5f);
+    }
+    public void Instruccions()
+    {
+        Application.LoadLevel("MenuInstrucciones");
+    }
+
+    public void ButtonAutors()
+    {
+        Invoke("Autors", 0.5f);
+    }
+    public void Autors()
+    {
+        Application.LoadLevel("MenuAutors");
+    }
+
+    public void ButtonMenu()
+    {
+        Invoke("Menu", 0.5f);
+    }
+    public void Menu()
+    {
+        Application.LoadLevel("MenuPrincipal");
+    }
+
+    public void ButtonPlay()
+    {
+        Invoke("Play",0.5f);
+    }
+    public void Play()
+    {
+        Globals.puntos = 0;
+        Globals.vidas = 3;
+        Application.LoadLevel("Level1");
+    }
+}
